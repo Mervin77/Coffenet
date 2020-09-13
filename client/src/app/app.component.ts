687137d8f1efe646504710aabe2d86584ca9f945
mervin77 <mervinsamurai@gmail.com>
2020-09-13 20:27:13 +0400

    End of section 9

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,4 @@
-import { Component, OnInit, ɵConsole } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import {IProduct} from './models/product';
-import {IPagination} from './models/pagination';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -10,17 +7,9 @@ import {IPagination} from './models/pagination';
 })
 export class AppComponent implements OnInit {
   title = 'CoffeeNet';
-  products: IProduct[];
 
-  constructor(private http: HttpClient) {}
+  constructor() {}
 
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe((Response:IPagination) =>{
-      this.products = Response.data;
-    }, error => {
-      console.log(error);
-
-    });
   }
-} 
-  
+}
